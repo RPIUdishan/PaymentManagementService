6cb489bb138c2aa2349b00059273f52229fa9411
RPIUdishan <it18082234@my.sliit.lk>
2020-05-04 11:34:30 +0530

    payment scheme

---
+++ src/com/PaymentSchemeAPI.java
@@ -0,0 +1,28 @@
+package com;
+
+import javax.ws.rs.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/PaymentSchemeAPI")
+public class PaymentSchemeAPI extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+}
